195000651c244714627151c5510c3c6581a845be
杨湍 <yangtuan@careate.cn>
2017-04-27 16:14:49 +0800

    第一次上传

---
+++ AuthorityOfCameraAndPhoto/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  AuthorityOfCameraAndPhoto
+//
+//  Created by chenyufeng on 6/20/16.
+//  Copyright © 2016 chenyufengweb. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "MainViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[MainViewController alloc] init]];
+    [self.window makeKeyAndVisible];
+
+    return YES;
+}
+
+@end
